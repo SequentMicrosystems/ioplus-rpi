bb29e9818fc4192c037c11d0a4b08bb7f9cc627d
alexburcea2877 <olcitu@gmail.com>
2022-01-10 02:15:28 +0000

    Add comment to leddim.py

--- python/examples/leddim.py
+++ python/examples/leddim.py
@@ -1,15 +1,16 @@
+#This example use open drain output to dimm a led or any DC load
 import libioplus as io
 import time
 
 try:
-	while True:
-		for i in range(100):
-			io.setOdPwm(0, 1, i * 100)
-			time.sleep(0.02)
-		for i in range(100):
-			io.setOdPwm(0, 1, (99 - i) * 100)
-			time.sleep(0.02)
-		time.sleep(1)		
+    while True:
+        for i in range(100):
+            io.setOdPwm(0, 1, i * 100)
+            time.sleep(0.02)
+        for i in range(100):
+            io.setOdPwm(0, 1, (99 - i) * 100)
+            time.sleep(0.02)
+        time.sleep(1)
 except KeyboardInterrupt:
-	pass
+    pass
 io.setOdPwm(0, 1, 0)
